068dd9ccd7286a45c3fd6b76605559c9f8aed3f7
Lars <larsmackenzie@gmail.com>
2019-07-01 15:34:43 -0400

    project setup

---
+++ src/components/Instructions/Instructions.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'; 
+
+class Instructions extends Component { 
+    render(){
+        return(
+            <div class="main">
+            <h3>What is LATTE for?</h3>
+            <p>LATTE is Brandeis University's learning management system. You will use LATTE for nearly everything in your online courses:&nbsp;</p><ul><li>storing learning materials</li><li>creating, collecting, and providing feedback on assignments</li><li>communicating with students&nbsp;</li><li>linking out to external resources and learning spaces</li></ul>
+            
+              <h3>What will I do in this orientation?</h3>
+            <p>This orientation contains a number of tasks for you to work through to get familiar with LATTE's tools - or to experiment with new ways of using them if you're already a pro. You'll use this sandbox course to learn how to add, modify, and interact with the suite of tools available to you through LATTE.&nbsp;</p>
+            <h3>Before you begin, there are a few things you should know how to do.</h3>
+            <p>We'll cover some of the basics of interacting with resources, activities, and students, but there are a few foundational concepts about LATTE that you should become familiar with first. Click through each link to learn about how to set up your profile (let students get to know you), upload a profile picture (and see you), and make your class available and accessible to students. You can also learn about some cool tricks (like how to preview what your course will look like to students). Be sure you're familiar with these foundational concepts before you move on to working on the tasks.</p>
+            <ul>
+              <li><a href="https://kb.brandeis.edu/display/LTS/Manage+Your+Personal+LATTE+Profile">Edit your LATTE profile</a></li>
+              <li><a href="https://ensemble.brandeis.edu/app/plugin/embed.aspx?playlistEmbed=true&amp;isResponsive=true&amp;isNewPluginEmbed=true&amp;displayTitle=true&amp;useIFrame=true&amp;destinationID=CzCMafo3ekKJ8dZF89KxJQ&amp;contentID=abdosSU22E6CPjVk3ZoHYg&amp;orderBy=videoTitle&amp;orderByDirection=asc&amp;pageIndex=3&amp;pageSize=10" target="_blank" rel="noopener noreferrer">Upload a profile picture</a></li>
+              <li><a href="https://kb.brandeis.edu/display/LTS/Make+Your+Course+Available+to+Students" target="_blank" rel="noopener noreferrer">Make your course available to students</a></li>
+              <li><a href="https://kb.brandeis.edu/display/LTS/Accessibility+Block+and+Toolbar" target="_blank" rel="noopener noreferrer">Add an Accessibility Toolbar</a></li><li><a href="https://www.lynda.com/Moodle-tutorials/Switch-roles-Moodle/628700/698502-4.html" target="_blank" rel="noopener noreferrer">View your course as a student</a></li>
+              <li><a href="https://www.lynda.com/Moodle-tutorials/Navigate-Moodle-course/511293/583690-4.html" target="_blank" rel="noopener noreferrer">Navigate through a LATTE course</a></li>
+            </ul>
+            <h3>I have the basics set up. What do I do now?</h3>
+          <p>If you scroll down on your page, you'll see four sections, guiding you through a series of tasks to complete to get you acclimated to LATTE. Make your way through each task before moving to the next section. Each section contains a place for you to add resources or interact with activities. Some of the tasks will entail creating activities and resources, while others will be learning about how you would do things that involve students (like creating groups, for instance) in an actual class. For a couple of tasks, you'll need to navigate to another section of LATTE to complete a task. You will see instructions on where to go to complete the task in the "Practice Here" sub-section.</p>
+          <p>At the very end of this page, you'll find a section called "Your Experimental Space." This is a blank topic where you can try out any tools not covered in this tutorial, or play with different layouts you might want to try in your course. Remember: this sandbox course cannot be copied to your course website, so any work you do here will not transfer to an actual course.</p>
+              <h3>Now, turn your <a href="https://docs.moodle.org/36/en/Turn_editing_on" target="_blank" rel="noopener noreferrer">editing on</a> and make your way through the orientation 	tasks!</h3>
+            </div>
+        )
+    }
+}
+export default Instructions; 
---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Card, CardColumns, Col, Row, Container, CardDeck } from 'react-bootstrap';
+class Resources extends Component {
+    render() {
+        return (
+            <div>
+                <div className="main">
+                    <h3>Course Resources</h3>
+                    <p className="center">Explore how to add course resources and adjust their settings in LATTE through practice. Take a look at each resource card and try to implement its task in the <a href="#practice1">Practice Here</a> section below.</p>
+                    <p className="center"><em>Stuck? Click on the "Need a hint?" link located on each card for help.</em></p><p className="center"><em>Don't forget to turn your <a href="https://docs.moodle.org/36/en/Turn_editing_on" target="_blank" rel="noopener noreferrer">editing on!</a></em></p>
+
+
+                    <Container>
+                        <Row noGutters className="flex-container">
+                            <Card border="primary" className="card-latte">
+                                <Card.Header>
+                                    <Card.Title className="card-title-container"><h5 className="card-title">Add a file<span className="breathing-room"><img src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/file.svg" alt="file icon" role="presentation" /></span></h5></Card.Title></Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <p className="card-text">Upload any File to your Sandbox course. <br /><br /><em>Try adding a few different file formats and adjusting the settings to hide and show the resource to students.</em></p>
+                                        <a href="https://docs.moodle.org/36/en/File_resource" className="blockquote-footer" target="_blank" rel="noopener noreferrer" >Need a hint?</a>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                            <Card border="primary" className="card-latte">
+                                <Card.Header>
+                                    <Card.Title className="card-title-container"><h5 className="card-title">Create a label<span className="breathing-room"><img src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/label.svg" alt="label icon" role="presentation" /></span></h5></Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <p className="card-text">Add an additional Label to the bottom of Topic of your Sandbox course.<br /><br /><em>Try embedding a video or image into the label and adding alt text.</em></p>
+                                        <a href="https://docs.moodle.org/36/en/Label" className="blockquote-footer" target="_blank" rel="noopener noreferrer">Need a hint?</a>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                            <Card border="primary" className="card-latte">
+                                <Card.Header>
+                                    <Card.Title className="card-title-container"><h5 className="card-title">Create a link<span className="breathing-room"><img src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/url.svg" alt="url icon" role="presentation" /></span></h5>
+                                    </Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <p className="card-text">Add a URL resource to your Sandbox course.<br /><br /><em>Try adjusting the settings to change how the URL opens for the user.</em></p>
+                                        <a href="https://docs.moodle.org/36/en/URL_resource"  className="blockquote-footer" target="_blank" rel="noopener noreferrer">Need a hint?</a>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+
+
+                        <div className="row spacer"></div>
+                        <Row noGutters className="flex-container">
+                            <Card border="primary" className="card-latte">
+                                <Card.Header>
+                                    <Card.Title className="card-title-container"><h5 className="card-title">Add a page<span className="breathing-room"><img src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/page.svg" alt="page icon" role="presentation" /></span></h5></Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                <Card.Text>
+                                    <p className="card-text">Create a new Page resource. Add some text and images or videos.<br /><br /><em> Try to use the Screenreader Help feature to learn how your page will display to people who use assistive technology.</em></p>
+                                    <a href="https://docs.moodle.org/36/en/Page_resource" className="blockquote-footer" target="_blank" rel="noopener noreferrer">Need a hint?</a>
+                                </Card.Text>
+                            </Card.Body>
+                                </Card>
+                                
+                        
+                            <Card border="primary" className="card-latte">
+                                <Card.Header>
+                                <Card.Title className="card-title-container"><h5 className="card-title">Create a folder<span className="breathing-room"><img src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/folder.svg" alt="folder icon" role="presentation" /></span></h5></Card.Title>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                    <p className="card-text">Add a Folder to help organize your resources.<br /><br /><em> Try adjusting how the folder's contents are displayed.</em></p>
+                                    <a href="https://docs.moodle.org/36/en/Folder_resource" className="blockquote-footer" target="_blank" rel="noopener noreferrer">Need a hint?</a>
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
+                   
+                        <Card border="primary" className="card-latte">
+                            <Card.Header>
+                            <Card.Title className="card-title-container"><h5 className="card-title">Form a group<span className="breathing-room"><img className="grade-img" src="https://moodle2.brandeis.edu/draftfile.php/1226073/user/draft/848202175/baseline-people_outline-24px.svg" alt="group icon" role="presentation" /></span></h5></Card.Title>
+                            </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                    <p className="card-text">Watch <a href="http://www.lynda.com/Moodle-tutorials/Creating-groups-collaborative-work/370012/406867-4.html?org=brandeis.edu" target="_blank" rel="noopener noreferrer">this video</a> to learn about how to add students to <a href="https://kb.brandeis.edu/display/LTS/Create+and+Use+Groups+in+a+Course" target="_blank" rel="noopener noreferrer">groups</a>. Then, create a group, give it a name, and add yourself to it.<br /><br /><em>Since you are the only enrolled user in this course, you'll have to wait to sort students into groups until you get your course LATTE page.</em></p>
+                                    <a href="https://docs.moodle.org/36/en/Groups" className="blockquote-footer" target="_blank" rel="noopener noreferrer">Need a hint?</a>
+                                    </Card.Text>
+                                </Card.Body>
+                        </Card>
+                    </Row>
+                    </Container>
+                </div >
+            </div >
+        );
+    }
+}
+export default Resources; 
